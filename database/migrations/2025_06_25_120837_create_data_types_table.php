44a07efa3a8733890d80d0b8efdcdc3d1b9f8946
imsoyfaygo <soyyak28@gmail.com>
2025-06-25 14:10:07 +0200

    Voyager Migrations

---
+++ database/migrations/2025_06_25_120837_create_data_types_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('display_name_singular');
+            $table->string('display_name_plural');
+            $table->string('icon')->nullable();
+            $table->string('model_name')->nullable();
+            $table->string('policy_name')->nullable();
+            $table->string('controller')->nullable();
+            $table->string('description')->nullable();
+            $table->boolean('generate_permissions')->default(false);
+            $table->tinyInteger('server_side')->default(0);
+            $table->text('details')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_types');
+    }
+};
---
+++ database/migrations/2025_06_25_120837_create_palmares_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('palmares', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('equipe');
+            $table->string('competition');
+            $table->year('annee');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('palmares');
+    }
+};
